b44d06691ab9a9c27575602484945035faf6c972
WELCOME <WELCOME@DESKTOP-P9RTI11>
2018-03-31 16:04:35 +0530

    First Commit

---
+++ javaTest/HelloWorld.java
@@ -0,0 +1,9 @@
+package javaTest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.print("First Java Code...");
+		System.out.println("Hii all");}
+}
